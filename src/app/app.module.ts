9825ab49a4abb42bd8f5e2a0f8ae49e25cde689c
mcr45 <depr3604@protonmail.com>
2023-09-28 23:44:40 -0500

    late night angular.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {ArticleComponent} from './article/article.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,ArticleComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/article/article.component.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core'
+
+@Component({
+selector:"article-app",
+templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+
+
+
+export class ArticleComponent{
+
+article:string='A nice article'
+
+
+}
